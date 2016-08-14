cde3055f4882167dd2b707fda2027f0325b50c8f
Vincent Lee <vincent.lee@outlook.com>
2016-08-14 17:46:09 -0500

    Tentatively done with randomwriter?

--- src/clojure_scratchpad/core.clj
+++ src/clojure_scratchpad/core.clj
@@ -3,7 +3,19 @@
   (:import (java.util Random)
            (java.util.regex Pattern)))
 
-(defn candidate-map
+(defn do-weighted-select
+  ([m rand]
+   (let [weights (zipmap (keys m) (reductions + (vals m))) ; TODO Not guaranteed to be ordered!
+         sel (.nextInt rand (second (last weights)))]
+     (key (first (drop-while #(<= (val %) sel) weights))))))
+
+(defn weighted-select
+  [m rand]
+  (if-not (seq m)
+    nil
+    (do-weighted-select (filter #(> (val %) 0) m) rand)))
+
+(defn do-candidate-map
   [seed input lvl]
   (if (= 0 lvl)
     (frequencies (drop 1 input))
@@ -12,6 +24,14 @@
          (filter not-empty)
          (map first)
          (frequencies))))
+
+(def memo-candidate-map (memoize do-candidate-map))
+
+(defn candidate-map
+  [seed input lvl]
+  (if (<= lvl 6)
+    (memo-candidate-map seed input lvl)
+    (do-candidate-map seed input lvl)))
 
 (defn rotate-seed
   [seed newchar]
@@ -30,7 +50,15 @@
   [input lvl length]
   (when (>= lvl (count input))
      (throw (IllegalArgumentException. "Level cannot be greater than input size")))
-  input)
+  (loop [output []
+        ctr 0
+        rand (Random.)
+        seed (initial-seed input lvl rand)]
+    (if (= ctr length)
+      (apply str output)
+      (if-let [c (weighted-select (candidate-map seed input lvl) rand)]
+        (recur (conj output c) (inc ctr) rand (rotate-seed seed c))
+        (recur (conj output \u0000) (inc ctr) rand (initial-seed input lvl rand))))))
 
 (defn -main
   [in out level length & args]

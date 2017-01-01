a9c22c01067d49265b982753d5c4f241b350abe5
Vincent Lee <vincent.lee@outlook.com>
2017-01-01 16:59:05 -0600

    Kahn's topological sort

---
+++ src/clojure_scratchpad/tsort.clj
@@ -0,0 +1,25 @@
+; Kahn 1962
+(ns clojure-scratchpad.tsort
+  (:import (clojure.lang PersistentQueue)))
+
+(def graph {:a #{:b :c}, :c #{:d :a}, :b #{:d}, :e #{:a :b}})
+
+(defn- dependents [g to-find]
+  (for [[k v] g :when (contains? v to-find)]
+    k))
+
+(defn- remove-edge [g from to]
+  (update g from #(disj % to)))
+
+(defn tsort [g]
+  (loop [wg g
+         result []
+         s (into PersistentQueue/EMPTY (filter #(empty? (dependents g %)) (keys g)))]
+    (if (seq s)
+      (let [n (peek s)
+            dependencies (get wg n)
+            new-graph (reduce #(remove-edge %1 n %2) wg dependencies)
+            to-add (filter #(empty? (dependents new-graph %)) dependencies)]
+        (recur new-graph (conj result n) (apply conj (pop s) to-add)))
+      (if (every? empty? (vals wg))
+        result :cycle))))
--- test/clojure_scratchpad/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-scratchpad.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-scratchpad.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
